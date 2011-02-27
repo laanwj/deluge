609251da269466f3f008de5a7231d4748b397956
Wladimir J. van der Laan <laanwj@gmail.com>
2011-02-27 12:20:54 +0100

    create patch by diffing branches

--- make_patch.sh
+++ make_patch.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 XIRVIK_PATH="/home/orion/projects/rentacoder/xirvik"
-git diff deluge-1.3.1 HEAD > ${XIRVIK_PATH}/patches-deluge/deluge.patch
+ORIG_VERSION="deluge-1.3.1"
+TO_VERSION="wumpus"
+
+git diff $ORIG_VERSION $TO_VERSION > ${XIRVIK_PATH}/patches-deluge/deluge.patch
 ( cd deluge/ui/web; bash build js/deluge-all )
 ( cd deluge/ui/web; bash build js/ext-extensions )
 tar -czvf ${XIRVIK_PATH}/js-deluge/deluge-all.tar.gz \
