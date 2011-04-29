8c2deedb2479d6b0532b8aad8fa019c022a3c05f
Wladimir J. van der Laan <laanwj@gmail.com>
2011-04-29 21:13:54 +0200

    always build JS

--- build_js.sh
+++ build_js.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+rm deluge/ui/web/js/deluge-all/.build_data
 ( cd deluge/ui/web; bash build js/deluge-all )
+rm deluge/ui/web/js/ext-extensions/.build_data
 ( cd deluge/ui/web; bash build js/ext-extensions )
