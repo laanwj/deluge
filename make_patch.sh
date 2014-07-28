66061c9ae4aa66b84bb791e13354d1a4839aef4f
Wladimir J. van der Laan <laanwj@gmail.com>
2014-07-28 16:50:08 +0200

    make patch per version

--- make_patch.sh
+++ make_patch.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
-XIRVIK_PATH="/home/orion/projects/rentacoder/xirvik"
+XIRVIK_PATH="$HOME/projects/rentacoder/xirvik"
+VER="1.3.7"
 ORIG_VERSION="deluge-1.3.7"
 TO_VERSION="master"
 
-git diff $ORIG_VERSION $TO_VERSION > ${XIRVIK_PATH}/patches-deluge/deluge.patch
+git diff $ORIG_VERSION $TO_VERSION > ${XIRVIK_PATH}/patches-deluge-$VER/deluge.patch
 rm -f deluge/ui/web/js/deluge-all/.build_data
 rm -f deluge/ui/web/js/ext-extensions/.build_data
 ( cd deluge/ui/web; bash build js/deluge-all )
 ( cd deluge/ui/web; bash build js/ext-extensions )
-tar -czvf ${XIRVIK_PATH}/js-deluge/deluge-all.tar.gz \
+tar -czvf ${XIRVIK_PATH}/js-deluge/deluge-all-$VER.tar.gz \
     deluge/ui/web/js/deluge-all.js \
     deluge/ui/web/js/deluge-all-debug.js \
     deluge/ui/web/js/ext-extensions.js \
